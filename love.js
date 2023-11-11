c64e0750a28ae7a56bd1c17552cd8a9d38723c8b
NTUT_Bernadette <100190023+bernadette451@users.noreply.github.com>
2023-11-11 21:33:56 +0800

    Create Path02 page and modify pages

---
+++ love.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById("game-container");
+var ctx = canvas.getContext("2d");
+// const ctx = canvas.getContext("2d");
+// ctx.beginPath();
+// ctx.moveTo(0,140);
+// ctx.lineTo(300,140);
+// ctx.stroke();
